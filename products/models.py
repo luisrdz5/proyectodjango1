d08aac2dfb6e4dc3e3f5080d0916b39b9c3f5d7e
Luis Rodriguez <luis_rdz5@hotmail.com>
2016-06-20 15:48:28 -0500

    Se ha agregado super usuario y caracteristicas al mismo

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+	descripcion = models.CharField(max_length=255)
+	categoria = models.CharField(max_length=255)
+	precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def __str__(self):
+		return self.name
+
+
