781df8403c5eaac6e61f09235252069d7eb8f5b5
Luis Rodriguez <luis_rdz5@hotmail.com>
2016-06-21 11:25:37 -0500

    Se genero una primera version del template

--- products/views.py
+++ products/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Product
 
 # Create your views here.
 
 def hello_world(request):
-	return HttpResponse('HELLO WORLD')
+	product = Product.objects.order_by('id')
+	template = loader.get_template('index.html')
+	context = {
+		'product' : product
+	}
+	return HttpResponse(template.render(context, request)) 
+	# return HttpResponse('HELLO WORLD')
+	# return render(request, 'index.html') 
